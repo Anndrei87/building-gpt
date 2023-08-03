cb59009478c1db3e79d237a763029fe9abe0c207
Anndrei87 <andrei09ka6@gmail.com>
2023-08-03 16:22:41 -0300

    feat: localizations done

--- lib/src/app_main.dart
+++ lib/src/app_main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'core/constants/constants.dart';
 import 'core/providers/chat_provider.dart';
+import 'core/providers/localization_provider.dart';
 import 'core/providers/models_provider.dart';
+import 'l10n/l10n.dart';
 import 'presentation/home_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,18 +16,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final l10n = L10n();
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (_) => ModelsProvider(),
         ),
         ChangeNotifierProvider(
+          create: (_) => LocalizationProvider(),
+        ),
+        ChangeNotifierProvider(
           create: (_) => ChatProvider(),
         ),
       ],
-      child: MaterialApp(
+      builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
         theme: ThemeData(
           scaffoldBackgroundColor: scaffoldbackground,
           appBarTheme: const AppBarTheme(
@@ -31,6 +39,14 @@ class MyApp extends StatelessWidget {
           ),
         ),
         home: const HomeScreen(),
+        supportedLocales: l10n.all,
+        locale: context.watch<LocalizationProvider>().getLocale,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
       ),
     );
   }
